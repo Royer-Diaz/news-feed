df302d50dc7197c105d1a67e7d9d08c083031371
Rogelio Diaz <rogeliod@thehangar.cr>
2020-03-05 16:26:07 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    flag: false
+  };
+
+  changeState = () => {
+    const { flag } = this.state;
+
+    this.setState({
+      flag: !flag
+    });
+  };
+
+  render() {
+    const { message, children } = this.props;
+    const { flag } = this.state;
+    const content = flag ? 'tiene contenido' : 'no tiene contenido';
+
+    if (flag) {
+      return <><h1>Flag es true</h1><button type="button" onClick={this.changeState}>Cambiar estado</button></>;
+    }
+
+    return (
+      <>
+        <h1>Hola Playo {message}</h1>
+        <h2>{children}</h2>
+        <p>{content}</p>
+        <button type="button" onClick={this.changeState}>Cambiar estado</button>
+      </>
+    );
+  }
 }
 
 export default App;
